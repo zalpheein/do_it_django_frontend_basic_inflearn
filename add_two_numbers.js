dd549c312a31babc5c89ac93a97a8dfb719979d2
xalphee <zalphee@gmail.com>
2021-09-01 16:01:07 +0900

    기초

---
+++ add_two_numbers.js
@@ -0,0 +1,10 @@
+function doSomthing() {
+    //alert("Hello World");
+    let a = document.getElementById("inputA").value;
+    let b = document.getElementById("inputB").value;
+
+    document.getElementById("valueA").innerHTML = a;
+    document.getElementById("valueB").innerHTML = b;
+    document.getElementById("valueC").innerHTML = Number(a) + Number(b);
+
+}
